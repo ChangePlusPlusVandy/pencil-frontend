5e3b0e23fc9153ae62bcbc286cad2120b2dc8b47
Kevin Jin <kevjin2002@gmail.com>
2021-11-09 20:27:05 -0600

    Added documenting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 
+/**
+ * Root component of application.
+ * 
+ * @returns {Object} - BrowserRouter with Router component.
+ * */
 const App = () => {
   return (
     <BrowserRouter>
--- src/AuthContext.js
+++ src/AuthContext.js
@@ -3,18 +3,41 @@ import firebase from "./firebase";
 
 const AuthContext = createContext();
 
+/**
+ * Allows for context to be imported in a page.
+ * 
+ * @returns {Object} - The context object.
+ * */
 export function useAuth() {
   return useContext(AuthContext);
 }
 
+/**
+ * Main functionality for AuthProvider.
+ * @param {Object} {children} - Children helpers/state of component.
+ * @returns {Object} - Created context with children helpers/states.
+ * */
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
+  /**
+   * Logs the user in with Firebase auth.
+   * @param {Object} {email} - Email of user.
+   * @param {Object} {password} - Password of user.
+   * @return {Object} - User object.
+   * */
   function login(email, password) {
     return firebase.auth().signInWithEmailAndPassword(email, password);
   }
 
+  /**
+   * Registers new user with Firebase auth.
+   * @param {Object} {name} - Name of user.
+   * @param {Object} {email} - Email of user.
+   * @param {Object} {password} - Password of user.
+   * @return {Object} - User object.
+   * */
   function register(name, email, password) {
     return firebase
       .auth()
@@ -26,19 +49,37 @@ export function AuthProvider({ children }) {
       });
   }
 
+  /**
+   * Logs the user out with Firebase auth.
+   * @return {Object} - User object.
+   * */
   function logout() {
     return firebase.auth().signOut();
   }
 
+  /**
+   * Checks if user is logged in with Firebase auth and gets credentials.
+   * @return {Object} - User object.
+   * */
   function getUser() {
     return firebase.auth().currentUser;
   }
 
+  /**
+   * Allows user to change password with email.
+   * @param {Object} {email} - Email of user.
+   * @return {Object} - User object.
+   * */
   function forgotPassword(email) {
     return firebase.auth().sendPasswordResetEmail(email);
   }
 
   useEffect(() => {
+    /**
+     * Updates user information in different parts of authentication.
+     * @param {Object} {user} - User credentials.
+     * @return {Object} - Function for cleanup.
+     * */
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       setCurrentUser(user);
       setIsLoading(false);
@@ -46,6 +87,7 @@ export function AuthProvider({ children }) {
     return unsubscribe;
   }, []);
 
+  
   const value = {
     currentUser,
     login,
