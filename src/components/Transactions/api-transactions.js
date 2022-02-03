e69e45a0d4038db7c169986720d25b9345b4766e
Kylie Burgess <burgess21k@gmail.com>
2022-02-02 19:37:09 -0500

    dashboard buttons

--- src/components/Transactions/api-transactions.js
+++ src/components/Transactions/api-transactions.js
@@ -1,26 +1,92 @@
 /**
  * Retrieves all transactions from Transaction database from Backend
- * 
+ *
  * @returns {Object} - All transaction objects with information about transaction
  */
+// const getAllTransactions = async () => {
+//   try {
+//     const response = await fetch(
+//       'http://localhost:8080/api/form/transaction/transactions'
+//     );
+
+//     if (!response.json().body.error) {
+//       return await response.json();
+//     }
+//     console.log('Error retrieving transactions');
+//     return { error: 'Error retrieving transactions' };
+//   } catch (err) {
+//     console.log(err);
+//     return { error: 'Teacher not found' };
+//   }
+// };
+
 const getAllTransactions = async () => {
   try {
     const response = await fetch(
       'http://localhost:8080/api/form/transaction/transactions'
     );
-
-    if (!response.json().body.error) {
-      return response.json();
-    }
-    else {
-      console.log("Error retrieving transactions");
-      return { error: "Error retrieving transactions"}
-    }
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return err;
   }
-  catch (err) {
+};
+
+const approveTransaction = async (data) => {
+  try {
+    const response = await fetch(
+      'http://localhost:8080/api/form/transaction/approve',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+      }
+    );
+    return await response.json();
+  } catch (err) {
     console.log(err);
     return { error: 'Teacher not found' };
   }
 };
 
-export { getAllTransactions }
+const denyTransaction = async (data) => {
+  // TODO: fix the URI here
+  try {
+    const response = await fetch(
+      'http://localhost:8080/api/form/transaction/deny',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+      }
+    );
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return { error: 'Teacher not found' };
+  }
+};
+
+const getTeacherByID = async (id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/form/teacher/${id}`
+    );
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return { error: 'Teacher Not Found' };
+  }
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export {
+  getAllTransactions,
+  approveTransaction,
+  denyTransaction,
+  getTeacherByID,
+};
