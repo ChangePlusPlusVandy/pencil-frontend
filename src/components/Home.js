89c2a9b976a0896fffd21648e571102e4586c1e4
Arthur Sung <arthursung98@gmail.com>
2022-02-07 20:21:54 -0600

    Header layout finished. TODO : change Dropdown css style using craco

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Menu/Menu.css';
 
-const Home = () => (
-  <div style={{ backgroundColor: '#f6f6f6' }}>
-    This is the home page
-    <br />
-    <Link to="/profile">Profile</Link>
-    <br />
-    <Link to="/inventory">Inventory</Link>
-    <br />
-    <Link to="/transactions">Transactions</Link>
-  </div>
-);
+const Home = () => <div>This is the Home Page</div>;
+
+export default Home;
