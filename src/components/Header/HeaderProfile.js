653962aca2cb24a2cc8bd33d04b0dcf5f1e4337e
Kevin Jin <kevjin2002@gmail.com>
2022-02-07 21:25:00 -0600

    remove antd

--- src/components/Header/HeaderProfile.js
+++ src/components/Header/HeaderProfile.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Dropdown, Menu } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
 import { useAuth } from '../../AuthContext';
-import 'antd/dist/antd.css';
 
 const HeaderProfile = () => {
   const { logout, getUser } = useAuth();
@@ -22,25 +19,26 @@ const HeaderProfile = () => {
     history.push('/');
   };
 
-  const menu = (
-    <Menu>
-      <Menu.Item key="0">
-        <a href="https://www.antgroup.com">Profile</a>
-      </Menu.Item>
-      <Menu.Item key="1">
-        <a href={handleLogout}>Log Out</a>
-      </Menu.Item>
-    </Menu>
-  );
+  // const menu = (
+  // <Menu>
+  //   <Menu.Item key="0">
+  //     <a href="https://www.antgroup.com">Profile</a>
+  //   </Menu.Item>
+  //   <Menu.Item key="1">
+  //     <a href={handleLogout}>Log Out</a>
+  //   </Menu.Item>
+  // </Menu>
+  // );
 
   return (
     <div className="header_profile">
-      <Dropdown overlay={menu} trigger={['click']}>
+      {/* <Dropdown overlay={menu} trigger={['click']}>
         <a className="ant-dropdown-link" href="/">
           Joel Wright
           <DownOutlined />
         </a>
-      </Dropdown>
+      </Dropdown> */}
+      Joel Wright
     </div>
   );
 };
