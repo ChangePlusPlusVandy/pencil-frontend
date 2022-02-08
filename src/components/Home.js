03fbed5a5c2a52f72c194d3057f879d855f4ccb7
Arthur Sung <arthursung98@gmail.com>
2022-02-07 20:31:11 -0600

    Merge cat-menu changes

--- src/components/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import Menu from './Menu/Menu';
-import { Link } from 'react-router-dom';
-import './Menu/Menu.css';
-
-const Home = () => <div>This is the Home Page</div>;
-
-export default Home;
