51d72ffc1c1a358e86cc94078674b5ea76b100b6
Zi Nean Teoh <zinean00@gmail.com>
2022-02-07 19:42:33 -0600

    implement general layout for Schedule

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -4,13 +4,30 @@ import { AiFillPrinter } from 'react-icons/ai';
 import './Schedule.css';
 
 const Schedule = () => {
-  const [changed, setChanged] = useState(false);
+  const [scheduleData, setScheduleData] = useState([]);
+
+  const handleDropdown = () => {
+    // handle dropdown between 'upcoming', ''
+  };
+
   return (
     <div className="scheduleContainer">
       <div className="scheduleHeader">
-        <h2>Schedule</h2>
-        <div className="scheduleButton">Print Schedule</div>
+        <h2>Schedule ({scheduleData.length})</h2>
+        <div className="inventoryButton">Print Schedule</div>
         <AiFillPrinter />
+
+        <select onChange={handleDropdown} className="dropdownButton">
+          <option name="upcoming">Upcoming</option>
+        </select>
+      </div>
+      <div className="itemContainer">
+        <div className="containerHeader">
+          <span>Time</span>
+          <span>Name</span>
+          <span>Phone</span>
+          <span>School</span>
+        </div>
       </div>
     </div>
   );
