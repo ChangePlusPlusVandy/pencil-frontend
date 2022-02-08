a2066652defc323b5dc3cfb476647b8ca2209660
Zi Nean Teoh <zinean00@gmail.com>
2022-02-07 19:24:52 -0600

    create Schedule folder and basic files

---
+++ src/components/Schedule/Schedule.js
@@ -0,0 +1,17 @@
+import React, { useState, Component } from 'react';
+import { AiFillPrinter } from 'react-icons/ai';
+
+import './Schedule.css';
+
+const Schedule = () => {
+  const [changed, setChanged] = useState(false);
+  return (
+    <div className="scheduleContainer">
+      <div className="scheduleHeader">
+        <h2>Schedule</h2>
+        <div className="scheduleButton">Print Schedule</div>
+        <AiFillPrinter />
+      </div>
+    </div>
+  );
+};
