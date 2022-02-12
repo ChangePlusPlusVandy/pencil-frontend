aa6c82d20353cdf7d3f9e2e68bcf1b9ce397c1a7
Intiser Rajoan Parash <irparash@gmail.com>
2022-02-12 12:30:20 -0600

    location dropdown and context added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,26 @@ import './Header.css';
 import PencilLogo from '../../img/pencil-logo-crop.png';
 
 import HeaderDropdown from './HeaderDropdown';
+import LocationDropdown from './LocationDropdown';
 
-const Header = () => (
-  <div className="header">
-    <div className="header_left">
-      <img src={PencilLogo} alt="Pencil Logo" className="header_logo" />
-      <p className="header_title">PENCIL-Nashville</p>
+import { useAuth } from '../../AuthContext';
+
+const Header = () => {
+  const { getCurrentLocation } = useAuth();
+  const location = `PENCIL-${getCurrentLocation()}`;
+
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img src={PencilLogo} alt="Pencil Logo" className="header_logo" />
+        <p className="header_title">{location}</p>
+      </div>
+      <div className="header_right">
+        <LocationDropdown />
+        <HeaderDropdown />
+      </div>
     </div>
-    <HeaderDropdown />
-  </div>
-);
+  );
+};
 
 export default Header;
