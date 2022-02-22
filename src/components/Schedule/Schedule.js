44246ff1a223b169bd445b558435844e72655073
catbat127 <catherine.yang127@gmail.com>
2022-02-21 19:50:43 -0600

    time added

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -19,6 +19,59 @@ const Schedule = () => {
     // handle dropdown between 'upcoming', ''
   };
 
+  const dateConverter = (date) => {
+    const year = date.slice(0, 4);
+    let month = parseInt(date.slice(5, 7), 10);
+    const day = date.slice(8, 10);
+    let hours = date.slice(11, 13);
+    const minutes = date.slice(14, 16);
+    let suffix = 'am';
+    // eslint-disable-next-line default-case
+    switch (month) {
+      case 1:
+        month = 'Jan';
+        break;
+      case 2:
+        month = 'Feb';
+        break;
+      case 3:
+        month = 'Mar';
+        break;
+      case 4:
+        month = 'Apr';
+        break;
+      case 5:
+        month = 'May';
+        break;
+      case 6:
+        month = 'June';
+        break;
+      case 7:
+        month = 'July';
+        break;
+      case 8:
+        month = 'Aug';
+        break;
+      case 9:
+        month = 'Sept';
+        break;
+      case 10:
+        month = 'Oct';
+        break;
+      case 11:
+        month = 'Nov';
+        break;
+      case 12:
+        month = 'Dec';
+        break;
+    }
+    if (hours > 12) {
+      suffix = 'pm';
+      hours -= 12;
+    }
+    return `${day} ${month} ${year}\n${hours}:${minutes} ${suffix}`;
+  };
+
   return (
     <div className="scheduleContainer">
       <div className="scheduleHeader">
@@ -43,9 +96,14 @@ const Schedule = () => {
             const fullName = item.name;
             const phoneNumber = item.phone;
             const schoolName = item.school;
+            const startTime = dateConverter(item.start_time);
+            const endTime = dateConverter(item.end_time);
             return (
               <div className="scheduleItem">
-                <td className="itemCell">hi</td>
+                <div className="timeBox">
+                  <td className="timeCell">{startTime}</td>
+                  <td className="timeCell">{endTime}</td>
+                </div>
                 <td className="itemCell">{fullName}</td>
                 <td className="itemCell">{phoneNumber}</td>
                 <td className="itemCell">{schoolName}</td>
