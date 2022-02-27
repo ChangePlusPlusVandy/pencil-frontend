145271668b1089f86cdbc144e33cbc3b4e9fea6f
Zi Nean Teoh <zinean00@gmail.com>
2022-02-26 21:19:46 -0600

    [refactor Item] more eslint

--- src/pages/Inventory/Item.js
+++ src/pages/Inventory/Item.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/mouse-events-have-key-events */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ReactComponent as HamburgerIcon } from '../../assets/HamburgerIcon.svg';
@@ -33,6 +31,8 @@ const Item = ({
         className="dragIcon"
         onMouseDown={() => setActive(true)}
         onMouseUp={() => setActive(false)}
+        role="button"
+        tabIndex={0}
       >
         <HamburgerIcon className="dragIcon" />
       </div>
