c7652b563c41fb50faf7fd9f741e44392ee32538
Intiser Rajoan Parash <irparash@gmail.com>
2022-03-17 01:26:29 -0500

    settings profile page finalized

--- src/pages/Settings/Profile.js
+++ src/pages/Settings/Profile.js
@@ -2,12 +2,64 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 import { HiPencil } from 'react-icons/hi';
+import { useAuth } from '../../AuthContext';
 
 const Profile = () => {
+  const {
+    currentUser,
+    changeDisplayName,
+    changePassword,
+    changeEmail,
+    reauthenticate,
+  } = useAuth();
   const [isProfileEditable, setIsProfileEditable] = useState(false);
   const [isPasswordEditable, setIsPasswordEditable] = useState(false);
-  const displayName = 'Joel Wright';
-  const email = 'joel.wright@pencil.org';
+  const [username, setUsername] = useState(currentUser.displayName);
+  const [email, setEmail] = useState(currentUser.email);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const checkPassword = async () => {
+    if (
+      currentPassword === '' ||
+      newPassword === '' ||
+      confirmPassword === ''
+    ) {
+      return false;
+    }
+    if (newPassword !== confirmPassword) {
+      setError('Passwords do not match');
+      return false;
+    }
+    if (currentPassword === newPassword) {
+      setError('New password must be different from old password');
+      return false;
+    }
+    try {
+      await reauthenticate(currentPassword);
+      await changePassword(newPassword);
+      return true;
+    } catch (err) {
+      setError(err.message);
+      return false;
+    }
+  };
+
+  const handleSave = () => {
+    if (username !== currentUser.displayName) {
+      changeDisplayName(username);
+    }
+    if (email !== currentUser.email) {
+      changeEmail(email);
+    }
+    if (checkPassword()) {
+      // if password change successful
+      setIsProfileEditable(false);
+    }
+    setIsProfileEditable(false);
+  };
 
   return (
     <div className="settingsBody">
@@ -16,75 +68,99 @@ const Profile = () => {
           <div className="profileColHeader">
             <h3>Edit Profile</h3>
             <div
-              className={`editButton ${
-                isProfileEditable ? 'editButtonSelected' : ''
+              className={`secondaryButton vertical-align-center ${
+                isProfileEditable ? 'secondaryButtonSelected' : ''
               }`}
-              onClick={() => setIsProfileEditable(true)}
+              onClick={() => setIsProfileEditable(!isProfileEditable)}
             >
-              <p>Edit</p>
+              Edit
               <HiPencil />
             </div>
           </div>
-          <div className="profileEntryTitle">
-            <p>Display name</p>
-            <input
-              value={displayName}
-              className="profileInput"
-              disabled={!isProfileEditable}
-            />
-          </div>
-          <div className="profileEntryTitle">
-            <p>Email</p>
-            <input
-              value={email}
-              className="profileInput"
-              disabled={!isProfileEditable}
-            />
+          <div
+            className={`${
+              isProfileEditable
+                ? 'profileInputContainer'
+                : 'profileInputContainerDisabled'
+            }`}
+          >
+            <div className="profileEntryTitle">
+              Display name
+              <input
+                className="profileInput"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                disabled={!isProfileEditable}
+              />
+            </div>
+            <div className="profileEntryTitle">
+              Email
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="profileInput"
+                disabled={!isProfileEditable}
+              />
+            </div>
           </div>
         </div>
         <div className="profileCol">
           <div className="profileColHeader">
             <h3>Change Password</h3>
             <div
-              className={`editButton ${
-                isPasswordEditable ? 'editButtonSelected' : ''
+              className={`secondaryButton ${
+                isPasswordEditable ? 'secondaryButtonSelected' : ''
               }`}
-              onClick={() => setIsPasswordEditable(true)}
+              onClick={() => setIsPasswordEditable(!isPasswordEditable)}
             >
-              <p>Edit</p>
+              Edit
               <HiPencil />
             </div>
           </div>
-          <div className="profileEntryTitle">
-            <p>Current password</p>
-            <input
-              placeholder=""
-              className="profileInput"
-              disabled={!isPasswordEditable}
-            />
-          </div>
-          <div className="profileEntryTitle">
-            <p>New password</p>
-            <input
-              placeholder=""
-              className="profileInput"
-              disabled={!isPasswordEditable}
-            />
-          </div>
-          <div className="profileEntryTitle">
-            <p>Confirm password</p>
-            <input
-              placeholder=""
-              className="profileInput"
-              disabled={!isPasswordEditable}
-            />
+          <div
+            className={`${
+              isPasswordEditable
+                ? 'profileInputContainer'
+                : 'profileInputContainerDisabled'
+            }`}
+          >
+            <div className="profileEntryTitle">
+              Current password
+              <input
+                type="password"
+                className="profileInput"
+                disabled={!isPasswordEditable}
+                value={currentPassword}
+                onChange={(e) => setCurrentPassword(e.target.value)}
+              />
+            </div>
+            <div className="profileEntryTitle">
+              New password
+              <input
+                type="password"
+                className="profileInput"
+                disabled={!isPasswordEditable}
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+            </div>
+            <div className="profileEntryTitle">
+              Confirm password
+              <input
+                type="password"
+                className="profileInput"
+                disabled={!isPasswordEditable}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
           </div>
 
           {/* Cancel and Save Buttons */}
           {(isPasswordEditable || isProfileEditable) && (
             <div className="profileActionButton">
               <div
-                className="editButton"
+                className="secondaryButton"
                 onClick={() => {
                   setIsPasswordEditable(false);
                   setIsProfileEditable(false);
@@ -92,11 +168,23 @@ const Profile = () => {
               >
                 Cancel
               </div>
-              <button type="button" className="saveButton">
+              <button
+                type="button"
+                className="primaryButton"
+                disabled={
+                  username === currentUser.displayName &&
+                  email === currentUser.email &&
+                  (currentPassword === '' ||
+                    newPassword === '' ||
+                    confirmPassword === '')
+                }
+                onClick={handleSave}
+              >
                 Save
               </button>
             </div>
           )}
+          {error && <p className="profileError">{error}</p>}
         </div>
       </div>
     </div>
