0c7b0c6c0737ed6e31f333ae8c1ee844287cbe11
Intiser Rajoan Parash <irparash@gmail.com>
2022-03-21 02:23:59 -0500

    Removed copy masterinventory from addLocation

--- src/components/Header/AddLocation.js
+++ src/components/Header/AddLocation.js
@@ -1,32 +1,18 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import './AddLocation.css';
-import { createNewLocation, getAllLocations } from './api-locations';
-import CustomCombobox from '../Combobox/CustomCombobox';
+import { createNewLocation } from './api-locations';
 
 const AddLocation = ({ show, onClose }) => {
   if (!show) return null;
-  const [locations, setLocations] = useState([]);
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
-  const [copy, setCopy] = useState(false);
-  const [copyLocation, setCopyLocation] = useState('');
-
-  useEffect(() => {
-    getAllLocations().then((data) => {
-      const locationNames = data.map((location) => location.name);
-      setLocations(locationNames);
-    });
-  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const formInfo = {
       name,
       address,
-      copy,
-      copyLocation,
     };
     createNewLocation(formInfo).then((res) => {
       console.log('Location created: ', res);
@@ -36,8 +22,8 @@ const AddLocation = ({ show, onClose }) => {
   };
 
   return (
-    <div className="location-modal-backdrop">
-      <div className="location-modal-content">
+    <div className="modal">
+      <div className="modal-content">
         <form onSubmit={handleSubmit}>
           <label className="inputLabel">
             Location Name
@@ -57,28 +43,14 @@ const AddLocation = ({ show, onClose }) => {
               onChange={(e) => setAddress(e.target.value)}
             />
           </label>
-          <label className="inputLabel vertical-align-center">
-            <input
-              type="checkbox"
-              className="customCheckbox"
-              checked={copy}
-              onClick={() => setCopy(!copy)}
-            />
-            Copy master inventory from existing location
-          </label>
-          <CustomCombobox
-            data={locations}
-            disabled={!copy}
-            onChange={setCopyLocation}
-          />
-          <div className="location-button-group">
+          <div className="add-button-group">
             <button type="button" className="secondaryButton" onClick={onClose}>
               Cancel
             </button>
             <button
               type="submit"
               className="primaryButton"
-              disabled={!name || !address || (copy && !copyLocation)}
+              disabled={!name || !address}
             >
               Add Location
             </button>
