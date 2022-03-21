31e301d339fd6b141590bb0a60489ff8c392b4b3
Intiser Rajoan Parash <irparash@gmail.com>
2022-03-21 03:13:26 -0500

    Modal component updates

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,18 +1,23 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import './Modal.css';
 
 const Modal = ({ show, onClose, actionButton, children }) => {
   if (!show) return null;
 
   return (
     <div className="modal-backdrop">
-      <div className="modal-body">{children}</div>
-      <div className="modal-button-group">
-        <button type="button" className="secondaryButton" onClick={onClose}>
-          Close
-        </button>
-        {actionButton}
+      <div className="modal-body">
+        <div>
+          {children}
+          <div className="modal-button-group">
+            <button type="button" className="secondaryButton" onClick={onClose}>
+              Close
+            </button>
+            {actionButton}
+          </div>
+        </div>
       </div>
     </div>
   );
