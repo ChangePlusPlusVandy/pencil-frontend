da2c86dc2d9d9f959e536109146b1f7833f4a3f2
Intiser Rajoan Parash <irparash@gmail.com>
2022-03-22 23:54:16 -0500

    schedule page table styling

--- src/pages/Schedule/Schedule.js
+++ src/pages/Schedule/Schedule.js
@@ -69,41 +69,39 @@ const Schedule = () => {
         leftArea={leftItems}
         rightArea={rightItems}
       />
-      <table className="itemContainer">
-        <tr className="tableItem tableHeader">
-          <td className="headerCell">Time</td>
-          <td className="headerCell">Name</td>
-          <td className="headerCell">Pencil ID</td>
-          <td className="headerCell">School</td>
-          <td className="headerCell">Phone Number</td>
-        </tr>
-        <tr>
-          {scheduleData.map((item, index) => {
-            // const createdAt = new Date(item.created_at);
-            const fullName = item.name;
-            const phoneNumber = item.phone;
-            const schoolName = item.school;
-            const date = formatDate(new Date(item.start_time));
-            const time = formatTime(
-              new Date(item.start_time),
-              new Date(item.end_time)
-            );
-            const pencilId = 'N/A';
-            return (
-              <div className="tableItem">
-                <div className="timeBox">
-                  <td className="timeCell">{date}</td>
-                  <td className="timeCell bold">{time}</td>
-                </div>
-                <td className="itemCell bold">{fullName}</td>
-                <td className="itemCell">{pencilId}</td>
-                <td className="itemCell">{schoolName}</td>
-                <td className="itemCell">{phoneNumber}</td>
+      <div className="tableContainer">
+        <div className="tableItemHeader">
+          <div className="headerCell">Time</div>
+          <div className="headerCell">Name</div>
+          <div className="headerCell">Pencil ID</div>
+          <div className="headerCell">School</div>
+          <div className="headerCell">Phone Number</div>
+        </div>
+        {scheduleData.map((item, index) => {
+          // const createdAt = new Date(item.created_at);
+          const fullName = item.name;
+          const phoneNumber = item.phone;
+          const schoolName = item.school;
+          const date = formatDate(new Date(item.start_time));
+          const time = formatTime(
+            new Date(item.start_time),
+            new Date(item.end_time)
+          );
+          const pencilId = 'N/A';
+          return (
+            <div className="tableItem">
+              <div className="timeBox">
+                <div className="timeCell">{date}</div>
+                <div className="timeCell bold">{time}</div>
               </div>
-            );
-          })}
-        </tr>
-      </table>
+              <div className="itemCell bold">{fullName}</div>
+              <div className="itemCell">{pencilId}</div>
+              <div className="itemCell">{schoolName}</div>
+              <div className="itemCell">{phoneNumber}</div>
+            </div>
+          );
+        })}
+      </div>
     </PageContainer>
   );
 };
