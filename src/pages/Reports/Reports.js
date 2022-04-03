5320bb32b9b03206d038d7374e5b21de0d20af3c
Intiser Rajoan Parash <irparash@gmail.com>
2022-04-03 01:19:33 -0500

    reports data shows up on frontend, styling needed

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable import/no-unresolved */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaFileDownload } from 'react-icons/fa';
 import 'pikaday/css/pikaday.css';
 import './Reports.css';
@@ -12,21 +12,39 @@ import PageContainer from '../../components/PageContainer/PageContainer';
 import CustomDropdown from '../../components/Dropdowns/CustomDropdown';
 import CalendarInput from './CalendarInput';
 import FilterInput from './FilterInput';
+import { getReport1 } from './api-reports';
+import { parseDate } from '../../utils/timedate';
 
 const Reports = () => {
+  const [reportData, setReportData] = useState([]);
+  const [reportSummary, setReportSummary] = useState({});
   const [view, setView] = useState('Weekly');
   const [fromDate, setFromDate] = useState('');
   const [untilDate, setUntilDate] = useState('');
+  const [schoolFilter, setSchoolFilter] = useState('');
+
+  useEffect(() => {
+    getReport1(fromDate, untilDate, schoolFilter).then((data) => {
+      if (data && !data.error) {
+        setReportData(data.transactions);
+        setReportSummary(data.summary);
+      }
+    });
+  }, [fromDate, untilDate, schoolFilter]);
+
+  const formatDate = (dateObj) => {
+    const { date, month, year } = parseDate(dateObj);
+
+    return `${date} ${month} ${year}`;
+  };
 
   const menuOptions = ['Weekly', 'School', 'No Show']; // TODO: this needs to be updated
 
   const menu = (
     <>
-      {menuOptions
-        // .filter((option) => option !== view)
-        .map((option) => (
-          <a onClick={(e) => setView(e.target.innerText)}>{option}</a>
-        ))}
+      {menuOptions.map((option) => (
+        <a onClick={(e) => setView(e.target.innerText)}>{option}</a>
+      ))}
     </>
   );
 
@@ -67,15 +85,30 @@ const Reports = () => {
         </div>
         <div className="tableHeaderRight">{rightItems}</div>
       </div>
-      <div className="itemContainer">
-        <tr className="tableItem tableHeader">
-          <td className="headerCell">Date</td>
-          <td className="headerCell">Teacher Name</td>
-          <td className="headerCell">Email</td>
-          <td className="headerCell">School</td>
-          <td className="headerCell">Total Product Value</td>
-        </tr>
-        <tr>{/* data.map(...) */}</tr>
+      <div className="tableContainer">
+        <div className="tableItemHeader">
+          <div className="scheduleCol1">Date</div>
+          <div className="scheduleCol2">Teacher Name</div>
+          <div className="scheduleCol3">Email</div>
+          <div className="scheduleCol4">School</div>
+          <div className="scheduleCol5">Total Product Value</div>
+        </div>
+        <div>
+          {reportData.map((transaction) => {
+            const date = formatDate(new Date(transaction.createdAt));
+            const { firstName, lastName, email } = transaction.Teacher;
+            const schoolName = transaction.School.name;
+            return (
+              <div className="tableItem">
+                <div className="scheduleCol1">{date}</div>
+                <div className="scheduleCol2">{`${firstName} ${lastName}`}</div>
+                <div className="scheduleCol3">{email}</div>
+                <div className="scheduleCol4">{schoolName}</div>
+                <div className="scheduleCol5">{transaction.totalItemPrice}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </PageContainer>
   );
