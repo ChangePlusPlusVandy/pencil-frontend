8dd0c5bf42f800d1779dba4a928a3947255dffdc
Kylie Burgess <burgess21k@gmail.com>
2022-04-04 21:28:37 -0400

    starting the db

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
+import PageContainer from '../../components/PageContainer/PageContainer';
+import TableHeader from '../../components/TableHeader/TableHeader';
+import { useAuth } from '../../AuthContext';
 
-const Dashboard = () => (
-  <div>
-    <Link to="/">Back</Link>
-    Hello
-  </div>
-);
+const Dashboard = () => {
+  const { currentUser } = useAuth();
+  console.log(currentUser);
+  return (
+    <PageContainer>
+      {Date().getHours > 11 ? (
+        <TableHeader title={`Good morning, ${currentUser.displayName}!`} />
+      ) : (
+        <TableHeader title={`Good afternoon, ${currentUser.displayName}!`} />
+      )}
+      <div className="todo">
+        <h2>To-do</h2>
+        <button className="addTask" type="button">
+          +
+        </button>
+      </div>
+    </PageContainer>
+  );
+};
 
 export default Dashboard;
