c93ad4b037f9dd979069242f01973e0a376fdb67
Kylie Burgess <burgess21k@gmail.com>
2022-04-04 21:01:04 -0400

    starting

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,31 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
-import { getTransactions } from './api-dashboard';
-
-const PendingTransactions = () => {
-  useEffect(() => {
-    getTransactions().then((transaction) => {
-      console.log(transaction);
-      if (transaction.error) {
-        console.log(transaction.error);
-      } else {
-        console.log('yeah');
-      }
-    });
-  }, []);
-
-  return (
-    <div className="scrollingTransactions">
-      <h1>hey</h1>
-    </div>
-  );
-};
 
 const Dashboard = () => (
   <div>
     <Link to="/">Back</Link>
-    <PendingTransactions />
+    Hello
   </div>
 );
 
