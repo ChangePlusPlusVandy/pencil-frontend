243e19e043791c3ec5098814e098e114b5eeadad
Zi Nean Teoh <zinean00@gmail.com>
2022-04-09 13:03:50 -0500

    fix reports date range query by converting date to actual ISO format

--- src/pages/Reports/api-reports.js
+++ src/pages/Reports/api-reports.js
@@ -1,19 +1,10 @@
 /* eslint-disable consistent-return */
-const formatDateTime = (date) => {
-  // format startDate from month/day/year to year-month-day
-  // and add 00:00:00 to the end of the date
-  if (!date || date === '') return '';
-  const dateArray = date.split('/');
-  const dateFormatted = `${dateArray[2]}-${dateArray[0]}-${dateArray[1]}`;
-  const time = '00:00:00';
-  return `${dateFormatted}T${time}.000`;
-};
 
 const getGeneralReport = async (startDate, endDate, schoolId, location) => {
   try {
-    console.log(startDate, endDate, schoolId);
-    const from = formatDateTime(startDate);
-    const to = formatDateTime(endDate);
+    console.log('Generating General Report:', startDate, endDate, schoolId);
+    const from = startDate && new Date(startDate).toISOString();
+    const to = endDate && new Date(endDate).toISOString();
     const query = `startDate=${from}&endDate=${to}&school=${schoolId}`;
     const response = await fetch(`/api/${location}/reports/report1?${query}`);
     return await response.json();
@@ -24,8 +15,8 @@ const getGeneralReport = async (startDate, endDate, schoolId, location) => {
 
 const getProductReport = async (startDate, endDate, schoolId, location) => {
   try {
-    const from = formatDateTime(startDate);
-    const to = formatDateTime(endDate);
+    const from = startDate && new Date(startDate).toISOString();
+    const to = endDate && new Date(endDate).toISOString();
     const query = `startDate=${from}&endDate=${to}&school=${schoolId}`;
     const response = await fetch(`/api/${location}/reports/report4?${query}`);
     return await response.json();
@@ -36,8 +27,8 @@ const getProductReport = async (startDate, endDate, schoolId, location) => {
 
 const getReport5 = async (startDate, endDate, schoolId, location) => {
   try {
-    const from = formatDateTime(startDate);
-    const to = formatDateTime(endDate);
+    const from = startDate && new Date(startDate).toISOString();
+    const to = endDate && new Date(endDate).toISOString();
     const query = `startDate=${from}&endDate=${to}&school=${schoolId}`;
     const response = await fetch(`/api/${location}/reports/report5?${query}`);
     return await response.json();
