0bc9a849485a635128d27dfba6748ea01a31d8a9
Intiser Rajoan Parash <irparash@gmail.com>
2022-04-20 00:40:11 -0500

    Skeleton setup of dashboard cards

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import PageContainer from '../../components/PageContainer/PageContainer';
+import { useAuth } from '../../AuthContext';
 import './Dashboard.css';
 
 const Dashboard = () => {
   const [info, setInfo] = useState([]);
+  const { currentUser } = useAuth();
+  const currentYear = new Date().getFullYear();
 
   return (
-    <div>
-      <h1>hey</h1>
-    </div>
+    <PageContainer>
+      <h1>Good afternoon, {currentUser.displayName.split(' ')[0]}! </h1>
+      <div className="dashboardContainer">
+        <div className="dashboardLeft">
+          <p>Today at a glance</p>
+          <div className="cardRegular">
+            <div>$12343</div>
+            <div>Total value donated</div>
+            <div>Icon</div>
+          </div>
+        </div>
+        <div className="dashboardRight">{currentYear} at a glance</div>
+      </div>
+    </PageContainer>
   );
 };
 
