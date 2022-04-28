b524e24be6f34a1cca39380855f5f75ba19f0db6
Kevin Jin <kevjin2002@gmail.com>
2022-04-28 17:42:14 -0500

    axios

--- src/pages/Dashboard/api-dashboard.js
+++ src/pages/Dashboard/api-dashboard.js
@@ -4,18 +4,13 @@ const getDailyStats = async (location) => {
   try {
     const todayStart = new Date().setHours(0, 0, 0);
     const todayEnd = new Date().setHours(23, 59, 59);
-    const response = await fetch(`api/${location}/dashboard/dailystats`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        startDate: todayStart,
-        endDate: todayEnd,
-      }),
+    console.log(todayStart);
+    const response = await axios.get(`/${location}/dashboard/dailystats`, {
+      startDate: todayStart,
+      endDate: todayEnd,
     });
 
-    return await response.json();
+    return response.data;
   } catch (err) {
     console.log(err);
     return err;
@@ -26,18 +21,12 @@ const monthlyStats = async (location) => {
   try {
     const monthStart = new Date().setDate(1);
     const monthEnd = new Date();
-    const response = await fetch(`api/${location}/dashboard/monthlystats`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        startDate: monthStart,
-        endDate: monthEnd,
-      }),
+    const response = await axios.get(`/${location}/dashboard/monthlystats`, {
+      startDate: monthStart,
+      endDate: monthEnd,
     });
 
-    return await response.json();
+    return response.data;
   } catch (err) {
     console.log(err);
     return err;
@@ -48,18 +37,12 @@ const getYearlyStats = async (location) => {
   try {
     const yearStart = new Date().setMonth(0, 1);
     const yearEnd = new Date().setMonth(11, 31);
-    const response = await fetch(`api/${location}/dashboard/yearlystats`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        startDate: yearStart,
-        endDate: yearEnd,
-      }),
+    const response = await axios.get(`/${location}/dashboard/yearlystats`, {
+      startDate: yearStart,
+      endDate: yearEnd,
     });
 
-    return await response.json();
+    return response.data;
   } catch (err) {
     console.log(err);
     return err;
