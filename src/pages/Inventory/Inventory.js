9cf2a3dd55e4ccfca17bce8e9567748399ac2a68
Kylie Burgess <burgess21k@gmail.com>
2022-04-30 18:05:39 -0400

    more inventory changes

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-useless-return */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable import/no-unresolved */
 import React, { useState, useEffect } from 'react';
 import {
   AiFillPrinter,
